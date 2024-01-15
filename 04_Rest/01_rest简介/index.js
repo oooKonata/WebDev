50068af273dbe3359960ca7835bd2bcb7b9659cd
oooKonata <Konata.ppoi@gmail.com>
2024-01-15 23:34:51 +0800

    REST 增删改查

--- 04_Rest/01_rest简介/index.js
+++ 04_Rest/01_rest简介/index.js
@@ -13,7 +13,7 @@ const STU_ARR = [
     { id: "3", name: "沙悟净", age: 38, gender: "男", address: "流沙河" }
 ]
 
-// 查询学生路由
+// 查询全部学生路由
 app.get("/students", (req, res) => {
     res.send({
         status: "OK",
@@ -36,6 +36,60 @@ app.post("/students", (req, res) => {
         status: "OK",
         data: stu
     })
+})
+
+// 查询某个学生路由
+app.get("/students/:id", (req, res) => {
+    const id = req.params.id
+    const stu = STU_ARR.find(stu => stu.id === id)
+    res.send({
+        status: "OK",
+        data: stu
+    })
+})
+
+// 删除学生路由
+app.delete("/students/:id", (req, res) => {
+    const id = req.params.id
+    for (let i = 0; i < STU_ARR.length; i++) {
+        if (STU_ARR[i].id === id) {
+            const stu = STU_ARR[i]
+            STU_ARR.splice(i, 1)
+            res.send({
+                status: "OK",
+                data: stu
+            })
+            return
+        }
+    }
+    // 学生不存在，执行以下代码
+    res.status(403).send({
+        status: "err",
+        data: "学生id不存在"
+    })
+})
+
+// 修改学生路由
+app.put("/students", (req, res) => {
+    const { id, name, age, gender, address } = req.body
+    const stu = STU_ARR.find(stu => stu.id === id)
+    if (stu) {
+        stu.name = name
+        stu.age = age
+        stu.gender = gender
+        stu.address = address
+
+        res.send({
+            status: "OK",
+            data: stu
+        })
+    } else {
+        res.status(403).send({
+            status: "err",
+            data: "学生信息不存在"
+        })
+    }
+
 })
 
 app.listen(3000, () => {
