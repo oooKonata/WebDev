5b740806c532b4592a8be684030bb0fc102dac34
oooKonata <Konata.ppoi@gmail.com>
2024-01-26 16:05:35 +0800

    内置属性ref

---
+++ 02_Vue/my_vue/备份/011_watchEffect/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        <!-- 使用组件 -->
+        <Person />
+    </div>
+</template>
+
+<script lang="ts" setup name="App">
+    import Person from './components/Person.vue'
+</script>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+</style>
