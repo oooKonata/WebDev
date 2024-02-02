fd60ae87fbb490a4404ab1200253cd3cd49594f4
oooKonata <Konata.ppoi@gmail.com>
2024-02-02 17:41:18 +0800

    路由query与params传参

---
+++ 02_Vue/my_vue/备份/017_路由_query传参/pages/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="news-list">
+    <li>编号：{{ query.id }}</li>
+    <li>标题：{{ query.title }}</li>
+    <li>内容：{{ query.content }}</li>
+  </ul>
+</template>
+
+<script setup lang="ts" name="Detail">
+import { useRoute } from 'vue-router'
+import { toRefs } from 'vue';
+// 获取当前路由地址
+let route = useRoute()
+// console.log(route.query);
+let { query } = toRefs(route)
+
+
+</script>
+
+<style scoped>
+.news-list {
+  list-style: none;
+  padding-left: 20px;
+}
+
+.news-list>li {
+  line-height: 30px;
+}
+</style>
