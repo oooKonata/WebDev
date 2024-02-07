199436c0c7bb9dade0d743ec4988d765008500b9
oooKonata <Konata.ppoi@gmail.com>
2024-02-07 18:01:07 +0800

    分页方法封装

---
+++ 03_syt/src/views/home/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <div>
+    <!-- 首页轮播 -->
+    <Carousel />
+    <!-- 首页搜索 -->
+    <Search />
+    <!-- 底部导航 -->
+    <el-row :gutter="20">
+      <el-col :span="20">
+        <!-- 筛选：医院等级 -->
+        <Level />
+        <!-- 筛选：医院地区 -->
+        <Region />
+        <!-- 医院展示（卡片） -->
+        <div class="hospital">
+          <Card
+            v-for="item in pageNum"
+            :key="item"
+            :url="pageUrlArr[item - 1]"
+          />
+        </div>
+        <div class="pagination">
+          <!-- 分页器 -->
+          <el-pagination
+            v-model:current-page="currentPage"
+            v-model:page-size="pageSize"
+            :page-sizes="[2, 4, 6, 8]"
+            layout="total, sizes, prev, pager, next, jumper"
+            :background="true"
+            :total="total"
+            @current-change="currentChange"
+            @size-change="sizeChange"
+          />
+        </div>
+      </el-col>
+      <el-col :span="4">222</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+  // 首页轮播
+  import Carousel from './carousel/index.vue'
+  // 首页搜索
+  import Search from './search/index.vue'
+  // 医院等级
+  import Level from './level/index.vue'
+  // 医院地区
+  import Region from './region/index.vue'
+  // 医院展示（卡片）
+  import Card from './card/index.vue'
+
+  import { ref, onMounted } from 'vue'
+  // 引入api reqCat
+  import { reqCat } from '@/api/home'
+  // 引入pagination hook
+  import pagination from '@/utils/pagination'
+
+  // 当前页数
+  let currentPage = ref(1)
+  // 每页显示的条目数
+  let pageSize = ref(2)
+  // 每页实际条目数
+  let pageNum = ref(2)
+  // 接口返回的全部图片url数组
+  let catUrlArr: any = ref([])
+  let total = ref(0)
+  // 每页图片url数组
+  let pageUrlArr: any = ref([])
+
+  // let totalNum: any = ref()
+  // 组件挂在后发起请求
+  onMounted(async () => {
+    // 获取图片url数组
+    await getCatUrl()
+    // 获取总条目数
+    total.value = catUrlArr.value.length
+    currentChange()
+  })
+
+  // 分页号改变时执行
+  const currentChange = () => {
+    // 使用封装的分页hook
+    const { pageNum: a, pageUrlArr: b } = pagination(
+      currentPage.value,
+      pageSize.value,
+      total.value,
+      catUrlArr.value
+    )
+    pageNum.value = a
+    pageUrlArr.value = b
+
+    /* console.log('current-page', currentPage.value, pageSize.value)
+    let orderNum: number = currentPage.value * pageSize.value
+    console.log('order', orderNum)
+    // 分页起始位数，考虑到有从0开始的情况，用末尾减去pageSize
+    let orderStart: number = orderNum - pageSize.value
+    let orderEnd: number
+    // 分页结束位数，考虑到最后一页条目数小于pageSize
+    if (orderNum < total.value) {
+      // 非最后一页时，结束为止等与起始位置 + pageSize
+      orderEnd = orderStart + pageSize.value
+    } else {
+      // 最后一页，结束位置是总数
+      orderEnd = total.value
+    }
+    pageNum.value = orderEnd - orderStart
+    console.log(orderStart, orderEnd, pageNum.value)
+    // 浅拷贝，取出url数组中指定的片段
+    pageUrlArr.value = catUrlArr.value.slice(orderStart, orderEnd) */
+  }
+
+  // 分页条目数改变时执行
+  const sizeChange = () => {
+    const x = pagination(
+      currentPage.value,
+      pageSize.value,
+      total.value,
+      catUrlArr.value
+    )
+    pageNum.value = x.pageNum
+    pageUrlArr.value = x.pageUrlArr
+  }
+  // 获取图片url数组
+  const getCatUrl = async () => {
+    let result: any = await reqCat()
+    // 接收接口返回对象的临时数组
+    let catUrlArr_tem = []
+    if (result.status == 200) {
+      for (let index = 0; index < result.data.length; index++) {
+        catUrlArr_tem.push(result.data[index].url)
+      }
+    }
+    // 临时数组给响应式url数组
+    catUrlArr.value = catUrlArr_tem
+  }
+</script>
+
+<style scoped lang="scss">
+  .hospital {
+    margin-top: 10px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+  .pagination {
+    margin: 20px 0;
+  }
+</style>
--- 03_syt/vite.config.ts
+++ 03_syt/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   // src文件夹配置别名“@”
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, 'src')
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
