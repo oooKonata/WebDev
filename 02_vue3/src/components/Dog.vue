d4bd54fa93feff7b6e38456eb44092594e63e0e6
oooKonata <Konata.ppoi@gmail.com>
2024-02-26 17:45:15 +0800

    内置特殊属性_ref

---
+++ 02_vue3/src/components/Dog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dog">
+    <div>dog name: {{ name }}</div>
+    <div>dog age: {{ age }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+  let name = ref('Yellow')
+  let age = ref(2)
+  defineExpose({ name, age })
+</script>
+
+<style scoped lang="scss">
+  .dog {
+    width: 30%;
+    padding: 10px;
+    margin: 25px 0;
+    border-radius: 8px;
+    background-color: cyan;
+  }
+</style>
---
+++ 02_vue3/备份/011_内置特殊属性_ref/components/Dog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="dog">
+    <div>dog name: {{ name }}</div>
+    <div>dog age: {{ age }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+  let name = ref('Yellow')
+  let age = ref(2)
+  // defineExpose将组件中的数据交给外部
+  defineExpose({ name, age })
+</script>
+
+<style scoped lang="scss">
+  .dog {
+    width: 30%;
+    padding: 10px;
+    margin: 25px 0;
+    border-radius: 8px;
+    background-color: cyan;
+  }
+</style>
